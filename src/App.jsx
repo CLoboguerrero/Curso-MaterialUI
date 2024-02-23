e65175f8f52180b0b84e7ce014ddf378c410abd4
CLoboguerrero <120610284+CLoboguerrero@users.noreply.github.com>
2024-02-23 13:30:50 -0600

    MUI packages installation and initial configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Importar un Button en mayúsculas, en automático está referenciando a un estilo de materialUI
+// Si se importa el botón como "@mui/material/Button", según el elemento cargaría mas rápido en fase de desarrollo
+import { Button } from "@mui/material"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>App</h1>
+      <Button variant="contained">Mi primer botón</Button>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+//Roboto font imports after installing it via npm:
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './index.css'
+import { CssBaseline } from '@mui/material';
+
+//Importante usar para que MUI se aplique en TODO el proyecto (Importarlo en un componente si sólo se requiere ahí):
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
 )
